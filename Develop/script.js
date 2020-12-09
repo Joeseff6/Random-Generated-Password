557e554b62dcdede3fa20df8bc1ab124b8a65900
Joseph Soria <josephsoria6@gmail.com>
2020-12-08 22:56:20 -0600

    Updated script.js to include prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+  prompt("Generate a password!\nDoes your password require lowercase characters?");
+  prompt("Choose any character to lowercase in your password:")
+  prompt("Does your password require uppercase characters?");
+  prompt("Choose any character to uppercase in your password:")
+  prompt("Does your password require numeric values?");
+  prompt("Choose any number to include in your password:")
+  prompt("Does your password require special characters?");
+  prompt("Choose any special characters to include in your password:")
+  prompt("How long does your password need to be? (Choose between 8 and 128 characters)");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +21,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
