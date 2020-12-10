6390202aa2695ef467304407bdeeaaf6b6d45ff5
Joseph Soria <josephsoria6@gmail.com>
2020-12-10 00:26:30 -0600

    Changed the name of variables in generatePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -11,37 +11,37 @@ var passwordText = {
 
 
 function generatePassword() {
-  choice1 = prompt(`Generate a password!\nDoes your password require lowercase characters?`).toLowerCase();
-  if (choice1 === 'yes') {
+  low = prompt(`Generate a password!\nDoes your password require lowercase characters?`).toLowerCase();
+  if (low === 'yes') {
     passwordText.lower = prompt(`Choose any character to lowercase in your password:`)
   };
 
-  
-  choice2 = prompt(`Does your password require uppercase characters?`).toLowerCase();
-  if (choice2 === 'yes') {
+
+  up = prompt(`Does your password require uppercase characters?`).toLowerCase();
+  if (up === 'yes') {
   passwordText.upper = prompt(`Choose any character to uppercase in your password:`)
   };
 
 
-  choice3 = prompt(`Does your password require numeric values?`);
-  if (choice3 === 'yes') {
+  numeric = prompt(`Does your password require numeric values?`);
+  if (numeric === 'yes') {
   passwordText.numerical = prompt(`Choose any number to include in your password:`)
   };
 
 
-  choice4 = prompt(`Does your password require special characters?`);
-  if (choice4 === 'yes') {
+  spec = prompt(`Does your password require special characters?`);
+  if (spec === 'yes') {
   passwordText.special = prompt(`Choose any special characters to include in your password:`)
   };
 
 
-  choice5 = parseInt(prompt(`How long does your password need to be?\n8 characters minimum, 128 characters max)`));
+  charLength = parseInt(prompt(`How long does your password need to be?\n8 characters minimum, 128 characters max)`));
   newNum = true;
   while (newNum === true) {
-    if (choice5 <= 8 || choice5 >= 128) {
+    if (charLength <= 8 || charLength >= 128) {
       newNum = true;
       alert(`Please enter a number in between 8 and 128.`);
-      choice5 = prompt(`How long does your password need to be?`);
+      charLength = prompt(`How long does your password need to be?`);
     } else {
       newNum = false;
     }
