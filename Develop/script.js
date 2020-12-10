b586b1ad69bddcc13eb7eca21709254e9c33e781
Joseph Soria <josephsoria6@gmail.com>
2020-12-10 00:22:45 -0600

    Added a while statement to ensure character length is between 8 and 128

--- Develop/script.js
+++ Develop/script.js
@@ -15,19 +15,38 @@ function generatePassword() {
   if (choice1 === 'yes') {
     passwordText.lower = prompt(`Choose any character to lowercase in your password:`)
   };
+
+  
   choice2 = prompt(`Does your password require uppercase characters?`).toLowerCase();
   if (choice2 === 'yes') {
   passwordText.upper = prompt(`Choose any character to uppercase in your password:`)
   };
+
+
   choice3 = prompt(`Does your password require numeric values?`);
   if (choice3 === 'yes') {
   passwordText.numerical = prompt(`Choose any number to include in your password:`)
   };
+
+
   choice4 = prompt(`Does your password require special characters?`);
   if (choice4 === 'yes') {
   passwordText.special = prompt(`Choose any special characters to include in your password:`)
   };
-  choice5 = prompt(`How long does your password need to be? (Choose between 8 and 128 characters)`);
+
+
+  choice5 = parseInt(prompt(`How long does your password need to be?\n8 characters minimum, 128 characters max)`));
+  newNum = true;
+  while (newNum === true) {
+    if (choice5 <= 8 || choice5 >= 128) {
+      newNum = true;
+      alert(`Please enter a number in between 8 and 128.`);
+      choice5 = prompt(`How long does your password need to be?`);
+    } else {
+      newNum = false;
+    }
+  }
+    
   return password = passwordText.lower + passwordText.upper + passwordText.numerical + passwordText.special
 }
 
