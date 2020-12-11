8e23d6a772f341261b732042c30cebb6ae18be8e
Joseph Soria <josephsoria6@gmail.com>
2020-12-10 18:37:58 -0600

    Added comments to annotate steps in writePassword()

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector(`#generate`);
 
+// Create an object to assign values via user prompt.
 var passwordText = {
   lower: ``,
   upper: ``,
@@ -9,32 +10,32 @@ var passwordText = {
   value: ``
 };
 
-
 function generatePassword() {
+    // User chooses if lower-cased characters are required.
   low = prompt(`Generate a password!\nDoes your password require lowercase characters?`).toLowerCase();
   if (low === 'yes') {
     passwordText.lower = prompt(`Choose any character to lowercase in your password:`)
   };
 
-
+  // User chooses if upper-cased characters are required.
   up = prompt(`Does your password require uppercase characters?`).toLowerCase();
   if (up === 'yes') {
   passwordText.upper = prompt(`Choose any character to uppercase in your password:`)
   };
 
-
+  // User chooses if numeric values are required.
   numeric = prompt(`Does your password require numeric values?`);
   if (numeric === 'yes') {
   passwordText.numerical = prompt(`Choose any number to include in your password:`)
   };
 
-
+  // User chooses if special characters are required.
   spec = prompt(`Does your password require special characters?`);
   if (spec === 'yes') {
   passwordText.special = prompt(`Choose any special characters to include in your password:`)
   };
 
-
+  // User chooses character length of password. User cannot choose anything outside of a number 8 through 128.
   charLength = parseInt(prompt(`How long does your password need to be?\n8 through 128 characters allowed)`));
   newNum = true;
   while (newNum === true) {
@@ -46,7 +47,8 @@ function generatePassword() {
       newNum = false;
     }
   }
-    
+
+  // Give a value to the password variable once the function is over.
   return password = passwordText.lower + passwordText.upper + passwordText.numerical + passwordText.special
 }
 
